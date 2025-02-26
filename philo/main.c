9cea5c33a2892f72ac596af335f824c4afda3f0b
tcarlier <carliertiti97459@gmail.com>
2025-02-26 05:58:50 +0100

    for testing

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 01:10:22 by tcarlier          #+#    #+#             */
-/*   Updated: 2025/02/26 05:56:33 by tcarlier         ###   ########.fr       */
+/*   Updated: 2025/02/26 05:58:20 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,10 @@ int	main(int ac, char **av)
 	t_table	table;
 	
 	if (ac < 5 || ac > 6)
-	{
 		error_exit("wrong input\n\t./philosophers nb_p t_d t_e t_s [m_e]\n");
-		parse_arg(&table, av);
-		init(&table);
-		dinner(&table);
-		clean(&table);
-	}
+	parse_arg(&table, av);
+	init(&table);
+	dinner(&table);
+	clean(&table);
 	return (0);	
 }
