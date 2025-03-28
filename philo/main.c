3438569c9d98dca86ef978e8c9b91bbfd8741cb1
Tristan Carlier <tcarlier@c2r8p8.42perpignan.fr>
2025-03-28 15:06:52 +0100

    all done

--- philo/main.c
+++ philo/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
+/*   By: tcarlier <tcarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 01:10:22 by tcarlier          #+#    #+#             */
-/*   Updated: 2025/02/26 06:30:45 by tcarlier         ###   ########.fr       */
+/*   Updated: 2025/03/28 15:05:21 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	main(int ac, char **av)
 	t_table	table;
 
 	if (ac < 5 || ac > 6)
-		error_exit("wrong input\n\t./philosophers nb_p t_d t_e t_s [m_e]\n");
+		error_exit("wrong input\nplease use :\t"
+			"./philo nb_philo time_die time_eat time_sleep [must_eat]\n");
 	parse_arg(&table, av);
 	init(&table);
 	dinner(&table);
